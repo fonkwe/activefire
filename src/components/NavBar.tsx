8ef57ca7bb8d91d7f7384212b6c95fe504a9131c
fonkwe <williajeni@gmail.com>
2022-06-27 03:56:45 -0500

    ActiveFire Project with TS

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className="top-nav" id="topest">
+    <h1>ActiveFires Project</h1>
+    <div className="links">
+    <a href="#yu">Contact</a>
+    </div>
+</div>
+  )
+}
