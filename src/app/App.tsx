e6aaf1d78ff839b0e0fbaa960a2514caea645cfa
fonkwe <williajeni@gmail.com>
2022-06-29 03:51:29 -0500

    Fixed folder structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import NavBar from './components/NavBar';
+import Header from '../components/Header';
+import NavBar from '../components/NavBar';
 
 function App() {
   return (
