07038afded9ea81e356d8ed22e970009d3d988ec
ljumbam <loayeh@gmail.com>
2022-07-09 10:34:21 -0700

    Remove component code from global css. It belongs in component css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div className="Map-container">
-    <h2 className="map-h2">Places With ActiveFires</h2>
+   <div>
+      <h2>Places With ActiveFires</h2>
    </div>
   )
 }
