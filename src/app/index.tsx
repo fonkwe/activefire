407d5c9ba25f06abff3f1b0ccb22fe2c26a309b1
fonkwe <williajeni@gmail.com>
2022-07-13 03:39:24 -0500

    just testing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './index.css';
 import Header from '../components/Header';
 import NavBar from '../components/NavBar';
+import MapContainer from '../components/Map';
 
 function App() {
   return (
     <div className="App">
        <NavBar />
        <Header />
+       <MapContainer height={500} width={500} />
     </div>
   );
 }
