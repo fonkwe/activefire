c9eaeb318e419bd0b8e7fc383f70da560c8429f6
fonkwe <williajeni@gmail.com>
2022-07-29 10:20:43 -0500

    implement a custom icon pointer

--- src/components/MapContainer/index.tsx
+++ src/components/MapContainer/index.tsx
@@ -10,6 +10,7 @@ import VectorSource from 'ol/source/Vector';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
 import * as olProj from 'ol/proj';
+import {Icon} from 'ol/style';
 
 import './index.css';
 
@@ -43,18 +44,25 @@ class MapContainer extends React.Component<MapContainerProps, MapContainerState>
         ],
         view: new View({
           center: [0, 0],
-          zoom: 2
+          zoom: 1
         })
       });
-
-
+      
+      const iconFeature = new Feature({
+        geometry: new Point(olProj.fromLonLat([4.35247, 50.84673])),
+        image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
+          anchor: [0.5, 46],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'pixels',
+          src: './fireImg,png',
+        }))
+      });
+      
+  
       var layer = new VectorLayer({
         source: new VectorSource({
-            features: [
-                new Feature({
-                    geometry: new Point(olProj.fromLonLat([4.35247, 50.84673]))
-                })
-            ]
+            features: [iconFeature]
+            
         })
     });
 
